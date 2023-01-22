dcef52825eade35f92d65a437d4c6e10caecfb2e
akkdev1 <akkaradechsr@gmail.com>
2023-01-22 19:55:57 +0700

    Build Landing page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-//import React from 'react'
+import React from 'react'
+import Landing from './pages/Landing'
 
 const App = () => {
   return (
     <div>
       <h1>JOBIFY</h1>
+      <Landing />
     </div>
   )
 }
