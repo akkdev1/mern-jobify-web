b58535a94596c33d4f8d29664eae031aa0d4ea5b
akkdev1 <akkaradechsr@gmail.com>
2023-01-22 22:56:23 +0700

    Error-> Register Page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React from 'react'
-import Landing from './pages/Landing'
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Dashboard, Landing, Register, Error } from './pages'
 
 const App = () => {
   return (
-    <div>
-      <h1>MERN</h1>
-      <Landing />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Dashboard />} />
+        <Route path='/register' element={<Register />} />
+        <Route path='/landing' element={<Landing />} />
+        <Route path='*' element={<Error />} />
+      </Routes>
+    </BrowserRouter >
   )
 }
 
